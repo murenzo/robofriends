ac228e187d608f47dd09ea097a8f8612399a13a4
Abdulazeez <seunbanjoko2005@yahoo.com>
2018-12-06 16:21:18 +0100

    Updates

---
+++ precache-manifest.4560d55cf11055d4cab74bf6b0f8dc2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5600967edd3f1a027665ba2572f23016",
+    "url": "/murenzo/robofriends/static/media/SEGA.5600967e.woff"
+  },
+  {
+    "revision": "39259da60a5d3a6a677a",
+    "url": "/murenzo/robofriends/static/js/runtime~main.39259da6.js"
+  },
+  {
+    "revision": "9467b037f656ec5670fe",
+    "url": "/murenzo/robofriends/static/js/main.9467b037.chunk.js"
+  },
+  {
+    "revision": "b3d0ef7f2a746e878cc7",
+    "url": "/murenzo/robofriends/static/js/1.b3d0ef7f.chunk.js"
+  },
+  {
+    "revision": "9467b037f656ec5670fe",
+    "url": "/murenzo/robofriends/static/css/main.cdd1616b.chunk.css"
+  },
+  {
+    "revision": "b3d0ef7f2a746e878cc7",
+    "url": "/murenzo/robofriends/static/css/1.b61e8c1e.chunk.css"
+  },
+  {
+    "revision": "94428bdd2662b90451f7af188f7d770a",
+    "url": "/murenzo/robofriends/index.html"
+  }
+];
